febf1189c2823c8e710590e2567174568f47fd18
ShivamP-97 <shivampathak19972@gmail.com>
2025-10-15 23:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // Your script here.
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+function setVoice() {
+  const selectedVoice = voices.find(voice => voice.name === voicesDropdown.value);
+  if (selectedVoice) {
+    msg.voice = selectedVoice;
+    restart(); 
+  }
+}
+
+function setOption() {
+  msg[this.name] = this.value;
+  restart(); 
+}
+
+function speak() {
+  msg.text = document.querySelector('[name="text"]').value;
+  if (!msg.text.trim()) return;
+  speechSynthesis.speak(msg);
+}
+
+function stop() {
+  speechSynthesis.cancel();
+}
+
+function restart() {
+  stop();
+  speak();
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', speak);
+stopButton.addEventListener('click', stop);
+
+populateVoices();
